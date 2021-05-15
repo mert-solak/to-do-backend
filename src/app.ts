755e31fbf89250718f45a09039d533a40fc24ef5
mert-solak <solak.mert@hotmail.com>
2021-05-15 19:39:40 +0300

    refactor(*): app.ts refactored

--- src/app.ts
+++ src/app.ts
@@ -1,59 +1,36 @@
 import express from 'express';
 import path from 'path';
 import logger from 'morgan';
-import http from 'http';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { dbConfig } from './configs/db.config';
-import taskRouter from './routes/task';
+import { dbConfig, urlConfig, loggerConfig } from './shared/configs';
+
+import taskRouter from './modules/task/task.router';
+import { CustomError } from './shared/utils';
+import { CustomErrorsEnum } from './shared/enums';
 
 mongoose.connect(`mongodb://${dbConfig.username}:${dbConfig.password}@localhost:27017`, { useNewUrlParser: true });
 
 const app = express();
 
-app.use(logger('dev'));
+if (loggerConfig.isOpen) {
+  app.use(logger(loggerConfig.type));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/tasks', taskRouter);
+app.use(urlConfig.tasks, taskRouter);
 
-const port = process.env.PORT || '3000';
-
-const onError = (error: any) => {
-  if (error.syscall !== 'listen') {
-    throw error;
+app.use((err, req, res, next) => {
+  if (err && err.errorCode) {
+    res.status(err.status);
+    return res.send(err);
   }
 
-  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  next(new CustomError(CustomErrorsEnum.UNHANDLED_ERROR));
+});
 
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ';
-  console.log('Listening on ' + bind);
-};
-
-const server = http.createServer(app);
-
-server.on('error', onError);
-server.on('listening', onListening);
-
-server.listen(port);
-
-module.exports = app;
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import http from 'http';
+
+import app from './app';
+
+export const port = process.env.PORT || '3000';
+
+const onError = (error: any) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ';
+  console.log('Listening on ' + bind);
+};
+
+export const server = http.createServer(app);
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+server.listen(port);
