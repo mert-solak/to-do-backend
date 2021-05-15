f7d13d1749d984b6cf364695d115d19502beb050
mert-solak <solak.mert@hotmail.com>
2021-05-15 15:23:36 +0300

    initial commit

---
+++ src/models/task.model.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const blogSchema = new Schema(
+  {
+    userName: { type: String, required: true },
+    name: { type: String, required: true },
+    finishesAt: { type: Date, required: true },
+    description: { type: String },
+  },
+  { timestamps: true }
+);
